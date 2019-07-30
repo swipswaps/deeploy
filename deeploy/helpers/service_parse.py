d0a271cbb08acc036ca99db36724869d7df00cc4
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-30 09:43:27 +0100

    fix lint issues

--- deeploy/helpers/service_parse.py
+++ deeploy/helpers/service_parse.py
@@ -1,30 +1,40 @@
-'''
-    Determines the service sending the request using the content of the
-    request headers
- '''
+''' Returns the name of the service sending the request '''
+class ServiceParse:
+    '''
+        ServiceParse()
+        Example:
+        ServiceParse({}).get_service() -> string
 
-class Service_parse:
+        Determines the service sending the request using the content
+        of the request headers
+    '''
 
     def __init__(self, request):
-        self.headers = request.headers
+        self.headers = request['headers']
 
     def get_service(self):
         '''
             get_service() -> string
 
-            Return the name service making the request
+            Return the name of service making the request
         '''
         headers = self.headers
         if not headers:
-            return
+            return None
 
-        '''
-            github = ['X-GitHub-Event', X-GitHub-Delivery', 'X-Hub-Signature']
-            bitbucket = ['X-Event-Key', 'X-Hook-UUID', 'X-Request-UUID', 'X-Attempt-Number']
+        service = ''
+        # github = ['X-GitHub-Event', X-GitHub-Delivery', 'X-Hub-Signature']
+        # bitbucket = ['X-Event-Key', 'X-Hook-UUID', 'X-Request-UUID', 'X-Attempt-Number']
+        # (todo - gitlab, circleCI, travisCI)
+        if (
+                ('X-GitHub-Event' in headers) and
+                ('X-GitHub-Delivery' in headers) and
+                ('X-Hub-Signature' in headers)
+            ):
+            service = 'github'
+        elif (('X-Event-Key' in headers) and ('X-Hook-UUID' in headers)):
+            service = 'bitbucket'
+        else:
+            service = None
 
-            todo - gitlab, circleCI, travisCI
-        '''
-        if 'X-GitHub-Event' in headers and 'X-GitHub-Delivery' in headers and 'X-Hub-Signature' in headers:
-            return 'github'
-        elif 'X-Event-Key' in headers and 'X-Hook-UUID' in headers:
-            return 'bitbucket'
+        return service
