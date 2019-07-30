358216a29806aa722bca79bf4215fbddf4df64b4
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-07-30 12:59:37 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-  name='Deeploy',
+  name='deeploy',
   version='0.0.1',
   script=['say_hello.py'],
   author='Ukor, Jidechi Ekundayo',
   author_email='ukorjidechi@gmail.com',
   description='A minilist deployment script',
   keyword='Delopy Deployment deeploy',
-  url='', # project website
+  url='https://github.com/ukor/deeploy',  # project website
+  packages=find_packages('deeploy'),
+  python_requires='>=3.6.*',
+  project_urls={
+    'Documentation': '',
+    'Changelog': '',
+    'Issue Tracker': 'https://github.com/ukor/deeploy/issues',
+  },
 )
