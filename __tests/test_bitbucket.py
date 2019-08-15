17897da21ab93ac842a5762c7b868209843d7cde
Ukor Jidechi E <ukorjidechi@gmail.com>
2019-08-16 00:59:07 +0100

    More test case

--- __tests/test_bitbucket.py
+++ __tests/test_bitbucket.py
@@ -3,4 +3,12 @@ from deeploy.parseRequest.bitbucket import Bitbucket
 from .bitbucket_dummy_data import headers, pull_request
 
 def test_parser():
-    assert Bitbucket(headers, pull_request).parser(), "Test passed"
+    c = Bitbucket(headers, pull_request).parser()
+    assert c, "Test passed"
+    assert isinstance(c, dict), "Type dictionary"
+
+def test_handle_request():
+    pr = pull_request.get('pullrequest')
+    scm = pull_request['repository']['scm']
+    project_details = Bitbucket(headers, pull_request).handle_pull_request(pr, scm)
+    assert isinstance(project_details, (dict)), "Return value is type dict"
